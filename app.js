e110bec8027a07ca260739def7386507acb240d7
ridoy113 <almamounhridoy113@gmail.com>
2023-03-05 09:53:33 +0600

    project complte

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach( button =>{
+    button.addEventListener('click',()=>{
+        const faq = button.nextElementSibling;
+        const icon = button.children[1];
+
+        faq.classList.toggle('show');
+        icon.classList.toggle('rotate');
+    })
+} )
